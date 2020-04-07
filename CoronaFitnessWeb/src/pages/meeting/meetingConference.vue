61d903145b932242ba5e697a92bfb0585df1def7
pavel.skrinnik <pskrinnik@tickeron.com>
2020-04-07 22:38:01 +0300

    prevent accedental leaving conference

--- CoronaFitnessWeb/src/pages/meeting/meetingConference.vue
+++ CoronaFitnessWeb/src/pages/meeting/meetingConference.vue
@@ -1,22 +1,47 @@
 ﻿<template>
-    <FxMeetingConference v-if="currentToken" :token="currentToken" />
+    <FxMeetingConference v-if="currentToken" :token="currentToken"/>
 </template>
 
 <script>
     import {createNamespacedHelpers} from 'vuex';
     import FxMeetingConference from "../../components/meeting/conference/FxMeetingConference.vue";
+
     const {mapActions, mapGetters} = createNamespacedHelpers('meetings/conference');
-    
+
     export default {
         components: {FxMeetingConference},
         computed: mapGetters(['currentToken']),
-        methods: mapActions(['loadToken', 'cleanToken']),
+        methods: {
+            confirm() {
+                return confirm("Вы уверены, что хотите покинуть конференцию?");
+            },
+
+            ...mapActions(['loadToken', 'cleanToken'])
+        },
+        created() {
+            this.cleanToken();
+            window.onbeforeunload = () => {
+                let result = this.confirm();
+                if (!result) {
+                    this.cleanToken();
+                }
+                
+                return result;
+            };
+        },
         mounted() {
             this.loadToken(this.$route.params.id);
         },
-        
-        beforeRouteLeave() {
+
+        beforeRouteLeave(to, from, next) {
+            if (!this.confirm()) {
+                next(from);
+                return;
+            }
+            
+            window.onbeforeunload = null;
             this.cleanToken();
+            next();
         }
     }
 </script>
