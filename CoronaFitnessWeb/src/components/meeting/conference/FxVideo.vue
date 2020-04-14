19a80e6aa798b0bc3ed0183b86ca9b63c47e572f
pavel.skrinnik <screen0994@gmail.com>
2020-04-14 22:49:13 +0300

    display names under videos

--- CoronaFitnessWeb/src/components/meeting/conference/FxVideo.vue
+++ CoronaFitnessWeb/src/components/meeting/conference/FxVideo.vue
@@ -1,7 +1,10 @@
 ﻿<template>
-    <div class="fx-video-container" :id="connectionId" @click="onVideoClick">
-        <BIconPersonFill class="fx-video-icon" v-show="!streamManager.stream.hasVideo"/>
-        <video ref="videoElement" class="fx-video" v-show="streamManager.stream.hasVideo"/>
+    <div class="fx-video" :id="connectionId" @click="onVideoClick">
+        <BIconPersonFill class="fx-video__icon" v-show="!streamManager.stream.hasVideo"/>
+        <video ref="videoElement" class="fx-video__video" v-show="streamManager.stream.hasVideo"/>
+        <div class="fx-video__name">
+            {{userName}}
+        </div>
     </div>
 </template>
 
@@ -14,6 +17,12 @@
         props: {
             streamManager: Object,
             connectionId: String
+        },
+        
+        computed: {
+          userName() {
+              return this.streamManager.stream.connection.data;
+          }
         },
 
         methods: {
@@ -37,27 +46,48 @@
 
 <style lang="scss" scoped>
     .fx-video {
-        height: 100%;
-        width: 100%;
-        background-color: transparent;
-    }
-
-    .fx-video-container {
         position: relative;
         border: 5px solid transparent;
         border-radius: 2px;
         transition: border-color 0.2s;
-
+        
         &:hover {
             border-color: lightgreen;
+            
+            .fx-video__name {
+                opacity: 1;
+            }
+        }
+        
+        &__video {
+            height: 100%;
+            width: 100%;
+            background-color: transparent;
         }
 
-        .fx-video-icon {
+        &__icon {
             height: 100%;
             width: 100%;
             text-align: center;
             font-size: 100px;
             background-color: #f2f2f2;
+        }
+        
+        &__name {
+            position: absolute;
+            bottom: 0;
+            left: 0;
+            width: 100%;
+            
+            background: rgba(0, 0, 0, 0.5);
+            color: white;
+            
+            text-align: center;
+            height: 2em;
+            line-height: 2;
+            
+            opacity: 0;
+            transition: opacity .2s;
         }
     }
 </style>
